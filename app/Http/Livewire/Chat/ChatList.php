537ddece93ec5080b14fda2943110b48206ff2f2
[your <mohamed.boukbir@uit.ac.ma>
2023-05-15 11:00:09 +0100

    message note working

--- app/Http/Livewire/Chat/ChatList.php
+++ app/Http/Livewire/Chat/ChatList.php
@@ -43,6 +43,7 @@ $this->receiverInstance= null;
      }
     public function getChatUserInstance(Conversation $conversation, $request)
     {
+    //    dd($request->id); 
         # code...
         $this->auth_id = auth()->id();
         //get selected conversation 
@@ -55,7 +56,7 @@ $this->receiverInstance= null;
         }
 
         if (isset($request)) {
-        dd($this->receiverInstance->$request);
+            dd($this->receiverInstance->$request->id);
             return $this->receiverInstance->$request;
             # code...
         }
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,5 +1,6 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
+
     <div class="chatlist_header">
 
         <div class="title">
